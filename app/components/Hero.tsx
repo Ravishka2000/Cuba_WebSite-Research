35d3237594ada062424d201704ddeeb924d4b3ab
Ravishka2000 <gamageravishkadulshan2000@gmail.com>
2024-10-15 23:38:16 +0530

    Create NAvbar and Modify Hero Section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
-import { AuroraBackground } from './ui/aurora-background';
+import React from "react";
+import { AuroraBackground } from "./ui/aurora-background";
+import { TypewriterEffect } from "./ui/typewriter-effect";
 
 const Hero = () => {
+  const words = [
+    {
+      text: "Enhance",
+    },
+    {
+      text: "Your",
+    },
+    {
+      text: "Inner",
+    },
+    {
+      text: "Development",
+    },
+    {
+      text: "Skills.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+
   return (
-    <div className='bg-black'>
+    <div className="bg-black">
       <AuroraBackground>
-        <div className='text-center'>
-          <h1 className='text-6xl font-bold text-white'>CUBA</h1>
-          <p className='mt-4 text-lg text-gray-300'>Your journey starts here</p>
+        <div className="text-center">
+          <h1 className="text-9xl font-extrabold text-white">CUBA</h1>
+          <TypewriterEffect words={words} className="mt-4" />
+          <button className="mt-8 inline-flex h-16 px-20 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] font-bold text-2xl text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            Learn More
+          </button>
         </div>
       </AuroraBackground>
     </div>
   );
-}
+};
 
 export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "./components/Hero";
+import { FloatingNav } from "./components/ui/floating-navbar";
+import { navItems } from "./data";
 
 export default function Home() {
   return (
     <main>
       <div>
+        <FloatingNav navItems={navItems}/>
+        <Hero/>
         <Hero/>
       </div>
     </main>
