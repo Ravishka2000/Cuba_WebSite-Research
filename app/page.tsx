d25b6dec0da7b35a9b4df3d00bfe4828ed74cea8
Ravishka2000 <gamageravishkadulshan2000@gmail.com>
2024-10-16 13:00:24 +0530

    Develop Project Timeline

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Hero from "./components/Hero";
+import Milestone from "./components/Milestone";
 import ProjectScope from "./components/ProjectScope";
 import { FloatingNav } from "./components/ui/floating-navbar";
 import { navItems } from "./data";
 
 export default function Home() {
-  return (
-    <main>
-      <div>
-        <FloatingNav navItems={navItems}/>
-        <Hero/>
-        <ProjectScope/>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <div>
+                <FloatingNav navItems={navItems} />
+                <Hero />
+                <ProjectScope />
+                <Milestone />
+            </div>
+        </main>
+    );
 }
