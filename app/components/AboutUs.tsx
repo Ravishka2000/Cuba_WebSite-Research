0bc24ef38a973144a264141a068561355fb9464c
Pravini Wick <pravinipw15@gmail.com>
2024-10-18 23:36:12 +0530

    Initial  commit

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -10,86 +10,86 @@ const AboutUs = () => {
 
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 mt-10">
                 <Card>
-                    <CardImage src="/pravini.png" alt="Team Member" />
+                    <CardImage src="/NuwanSir.jpg" alt="Team Member" />
                     <div className="p-4">
-                        <CardTitle>Pravini Wickramanayake</CardTitle>
+                        <CardTitle>Dr. Nuwan Kodagoda</CardTitle>
                         <CardDescription>
                             Sri Lanka Institute of Information Technology <br />
-                            <strong>Department:</strong> Information Systems Engineering
+                            <strong>Department:</strong> Software Engineering 
                         </CardDescription>
                         <CardFooter>
                             <a href="https://scholar.google.com">LinkedIn</a>
-                            <a href="mailto:example@example.com">E-Mail</a>
+                            <a href="mailto:nuwan.k@sliit.lk">E-Mail</a>
                         </CardFooter>
                     </div>
                 </Card>
                 <Card>
-                    <CardImage src="/pravini.png" alt="Team Member" />
+                    <CardImage src="/JennyMiss.jpg" alt="Team Member" />
                     <div className="p-4">
-                        <CardTitle>Pravini Wickramanayake</CardTitle>
+                        <CardTitle>Ms. Jenny Krishara</CardTitle>
                         <CardDescription>
                             Sri Lanka Institute of Information Technology <br />
-                            <strong>Department:</strong> Information Systems Engineering
+                            <strong>Department:</strong> Software Engineering 
                         </CardDescription>
                         <CardFooter>
                             <a href="https://scholar.google.com">LinkedIn</a>
-                            <a href="mailto:example@example.com">E-Mail</a>
+                            <a href="mailto:jenny.k@sliit.lk">E-Mail</a>
                         </CardFooter>
                     </div>
                 </Card>
                 <Card>
-                    <CardImage src="/pravini.png" alt="Team Member" />
+                    <CardImage src="/Hiruna.jpg" alt="Team Member" />
                     <div className="p-4">
-                        <CardTitle>Pravini Wickramanayake</CardTitle>
+                        <CardTitle>Lalanga S.P.H</CardTitle>
                         <CardDescription>
                             Sri Lanka Institute of Information Technology <br />
-                            <strong>Department:</strong> Information Systems Engineering
+                            <strong>Department:</strong> Software Engineering 
                         </CardDescription>
                         <CardFooter>
                             <a href="https://scholar.google.com">LinkedIn</a>
-                            <a href="mailto:example@example.com">E-Mail</a>
+                            <a href="mailto:it21049590@my.sliit.lk">E-Mail</a>
                         </CardFooter>
                     </div>
                 </Card>
                 <Card>
-                    <CardImage src="/pravini.png" alt="Team Member" />
+                    <CardImage src="/Ravishka.jpg" alt="Team Member" />
                     <div className="p-4">
-                        <CardTitle>Pravini Wickramanayake</CardTitle>
+                        <CardTitle>Ranaweera G.R.D</CardTitle>
                         <CardDescription>
                             Sri Lanka Institute of Information Technology <br />
-                            <strong>Department:</strong> Information Systems Engineering
+                            <strong>Department:</strong> Software Engineering
                         </CardDescription>
                         <CardFooter>
                             <a href="https://scholar.google.com">LinkedIn</a>
-                            <a href="mailto:example@example.com">E-Mail</a>
+                            <a href="mailto:it21052606@my.sliit.lk">E-Mail</a>
                         </CardFooter>
                     </div>
                 </Card>
                 <Card>
-                    <CardImage src="/pravini.png" alt="Team Member" />
+                    <CardImage src="/Maneesha.jpg" alt="Team Member" />
                     <div className="p-4">
-                        <CardTitle>Pravini Wickramanayake</CardTitle>
+                        <CardTitle>Karunarathna K.M.P.M</CardTitle>
                         <CardDescription>
                             Sri Lanka Institute of Information Technology <br />
-                            <strong>Department:</strong> Information Systems Engineering
+                            <strong>Department:</strong> Information Technology
                         </CardDescription>
                         <CardFooter>
                             <a href="https://scholar.google.com">LinkedIn</a>
-                            <a href="mailto:example@example.com">E-Mail</a>
+                            <a href="mailto:it21060908@my.sliit.lk ">E-Mail</a>
                         </CardFooter>
                     </div>
                 </Card>
                 <Card>
-                    <CardImage src="/pravini.png" alt="Team Member" />
+                    <CardImage src="/Pravini.jpg" alt="Team Member" />
                     <div className="p-4">
-                        <CardTitle>Pravini Wickramanayake</CardTitle>
+                        <CardTitle>Wickramanayake V.P.P</CardTitle>
                         <CardDescription>
                             Sri Lanka Institute of Information Technology <br />
-                            <strong>Department:</strong> Information Systems Engineering
+                            <strong>Department:</strong> Software Engineering
                         </CardDescription>
                         <CardFooter>
                             <a href="https://scholar.google.com">LinkedIn</a>
-                            <a href="mailto:example@example.com">E-Mail</a>
+                            <a href="mailto:it21041402@my.sliit.lk">E-Mail</a>
                         </CardFooter>
                     </div>
                 </Card>
--- app/components/Downloads.tsx
+++ app/components/Downloads.tsx
@@ -15,11 +15,11 @@ const Downloads = () => {
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
+                            Topic Assessment
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
+                            Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
@@ -31,11 +31,11 @@ const Downloads = () => {
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
+                            Project Charter
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
+                            Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
@@ -47,11 +47,11 @@ const Downloads = () => {
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
+                        Project Proposal
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
+                            Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
@@ -63,11 +63,11 @@ const Downloads = () => {
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
+                        Status Documents I
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
+                            Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
@@ -79,11 +79,11 @@ const Downloads = () => {
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
+                        Status Documents II
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
+                            Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
@@ -95,11 +95,11 @@ const Downloads = () => {
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
+                        Research Paper
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
+                            Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
@@ -111,11 +111,11 @@ const Downloads = () => {
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
+                        Final Report
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
+                            Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
@@ -127,75 +127,11 @@ const Downloads = () => {
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
+                        Poster
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
-                            </span>
-                        </div>
-                        <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
-                    </div>
-                </PinContainer>
-                <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
-                >
-                    <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-                        <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
-                        </h3>
-                        <div className="text-base !m-0 !p-0 font-normal">
-                            <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
-                            </span>
-                        </div>
-                        <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
-                    </div>
-                </PinContainer>
-                <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
-                >
-                    <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-                        <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
-                        </h3>
-                        <div className="text-base !m-0 !p-0 font-normal">
-                            <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
-                            </span>
-                        </div>
-                        <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
-                    </div>
-                </PinContainer>
-                <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
-                >
-                    <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-                        <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
-                        </h3>
-                        <div className="text-base !m-0 !p-0 font-normal">
-                            <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
-                            </span>
-                        </div>
-                        <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
-                    </div>
-                </PinContainer>
-                <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
-                >
-                    <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-                        <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                            Aceternity UI
-                        </h3>
-                        <div className="text-base !m-0 !p-0 font-normal">
-                            <span className="text-slate-500 ">
-                                Customizable Tailwind CSS and Framer Motion Components.
+                            Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
