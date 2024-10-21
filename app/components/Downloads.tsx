a35a999a8fc98826032da239384e463a71828654
Hiruna Lalanga <IT21049590@my.sliit.lk>
2024-10-21 15:04:08 +0530

    added report download links

--- app/components/Downloads.tsx
+++ app/components/Downloads.tsx
@@ -11,8 +11,8 @@ const Downloads = () => {
 
             <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
                 <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
+                    title="Taf"
+                    href="https://drive.google.com/drive/folders/1G1D5CRy535MvFpYw51e7sUuXwP7XRUDN?usp=sharing"
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
@@ -20,16 +20,16 @@ const Downloads = () => {
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                            Submitted on 2024
+                                Submitted on 2024
                             </span>
                         </div>
-                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500"/>
-                           
+                        <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+
                     </div>
                 </PinContainer>
                 <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
+                    title="Project Charter"
+                    href="https://drive.google.com/drive/folders/14gCsOroAZh7PYDI1_N39pj5srsokll5V?usp=sharing"
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
@@ -37,103 +37,103 @@ const Downloads = () => {
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                            Submitted on 2024
+                                Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
                     </div>
                 </PinContainer>
                 <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
+                    title="Prpposal"
+                    href="https://drive.google.com/drive/folders/1B9ezbXgKzjl6V6or9T3MQX6O2buLGIaZ?usp=sharing"
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                        Project Proposal
+                            Project Proposal
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                            Submitted on 2024
+                                Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
                     </div>
                 </PinContainer>
                 <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
+                    title="Status Documents I"
+                    href="https://drive.google.com/drive/folders/1caWrLXdujRePPqC1crMYUqDr44siC-Yz?usp=sharing"
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                        Status Documents I
+                            Status Documents I
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                            Submitted on 2024
+                                Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
                     </div>
                 </PinContainer>
                 <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
+                    title=" Status Documents II"
+                    href="https://drive.google.com/drive/folders/1jYMqfHm3V19R7z-8-uRDjSBlpSZlH8SV?usp=sharing"
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                        Status Documents II
+                            Status Documents II
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                            Submitted on 2024
+                                Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
                     </div>
                 </PinContainer>
                 <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
+                    title="Research Paper"
+                    href="https://drive.google.com/drive/folders/1tcCegApYY8TaS13mURkYylf5WyFs7Ll2?usp=sharing"
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                        Research Paper
+                            Research Paper
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                            Submitted on 2024
+                                Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
                     </div>
                 </PinContainer>
                 <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
+                    title="Final Report"
+                    href="https://drive.google.com/drive/folders/1PGZcbMRySGfSEaUh6ZSWhRVHC1-W0QAu?usp=sharing"
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                        Final Report
+                            Final Report
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                            Submitted on 2024
+                                Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
                     </div>
                 </PinContainer>
                 <PinContainer
-                    title="/ui.aceternity.com"
-                    href="https://twitter.com/mannupaaji"
+                    title="Poster"
+                    href="https://drive.google.com/drive/folders/1Du2nq6utboqh6h4cmZwmmlQHOn555-Q3?usp=sharing"
                 >
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                        Poster
+                            Poster
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                            Submitted on 2024
+                                Submitted on 2024
                             </span>
                         </div>
                         <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
